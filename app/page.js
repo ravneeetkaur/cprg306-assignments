22dddae1b05bbdc142997d968b3dcb0c8f79b87e
ravneeetkaur <kaur.ravneet@edu.sait.ca>
2025-05-21 20:26:34 -0600

    updated final code
    This is the updated final code for week 2

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ export default function Home() {
     <main>
       <h1>CPRG 306: Web Development 2 - Assignments</h1>
       <p>
-        <Link href="/week-2"> GO to Week 2 Assignment</Link>
+        <Link href="/week-2"> Go to Week 2 Assignment</Link>
       </p>
     </main>
 
